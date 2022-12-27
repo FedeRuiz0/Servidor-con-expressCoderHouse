749056854cd938d1ac8379d22cf8a3e1d3c6565e
FedeRuiz <fedenox0@gmail.com>
2022-12-27 04:11:53 -0300

    arreglé unas cosas

--- server.js
+++ server.js
@@ -3,21 +3,34 @@ const fs = require('fs');
 const app = express();
 
 app.get('/products', (req, res) => {
-    fs.readFile('./products/products.json', 'utf8', (err, data) => {
-      if (err) throw err;
-      res.send(JSON.parse(data));
-    });
+  fs.readFile('./products/products.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Error al leer el archivo de productos' });
+      return;
+    }
+    res.send(JSON.parse(data));
   });
-  
-  app.get('/',(req, res) => {
-    res.send('/products')
 });
-  
+
+app.get('/', (req, res) => {
+  fs.readFile('./products/products.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ message: 'Error al leer el archivo de productos' });
+      return;
+    }
+    res.send(JSON.parse(data));
+  });
+});
+
+
 const PORT = 8080;
 
-const server = app.listen(PORT, () => {
-    console.log(`Servidor http escuchado en el puerto ${server.address().port}`)
-})
+app.listen(PORT, () => {
+  console.log(`Servidor http escuchado en el puerto ${PORT}`);
+});
+
 
 
 
